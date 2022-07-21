16976258c10fbe928ecf55be7c9b8c22285b4c81
Raisul999 <raisulislamarnob0@gmail.com>
2022-07-21 15:20:16 +0600

    backend

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-const app = require('express')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
+dotenv.config();
+const PORT = 5000 || process.env.PORT
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("Connected to DB"))
+.catch((err)=>console.log(err))
+app.use(express.json())
+app.use('/api', userRoute)
+app.use('/api/auth', authRoute)
+app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
+
+
---
+++ server/models/Cart.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const CartSchema = new mongoose.Schema(
+    {
+        userId: { type: String, required: true},
+        products: [
+            {
+                products:{type: String},
+                quantity:{type:Number, default: 0},
+            }
+        ]
+        
+    },
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model('Cart', CartSchema)
